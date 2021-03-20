d1fba9c02a549b175e8f6ae23001073669562383
Oksana-png <73472712+Oksana-png@users.noreply.github.com>
2021-03-21 00:04:11 +0500

    create: site and more fails

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+
+});
